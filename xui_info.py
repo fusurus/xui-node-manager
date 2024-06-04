9368c62ba25de3482cb85bb4eece1a9b5189bb40
扶苏如是 <57750156+fusurus@users.noreply.github.com>
2024-06-05 00:26:37 +0800

    Update xui_info.py

--- xui_info.py
+++ xui_info.py
@@ -1,3 +1,4 @@
+import logging
 import requests
 import random
 import json
@@ -30,15 +31,17 @@ class XUIManager:
         try:
             response = self.session.post(f'http://{ip}:{self.port}/login', headers=headers, data=data, verify=False)
             if response.status_code != 200:
-                print(f"{ip} 登录失败")
+                logging.error(f"{ip} 登录失败")
                 return False
-            print(f"{ip} 登录成功")
+            logging.info(f"{ip} 登录成功")
             return True
         except Exception as e:
-            print(f"{ip} 登录失败: {e}")
+            logging.error(f"{ip} 登录失败: {e}")
             return False
 
-    def add_inbound(self, ip, remark, idx, client_id):
+    def add_inbound(self, ip, remark, client_id, idx=None):
+        if idx is None:
+            idx = 0  # 设置默认值，例如从0开始
         headers = {
             'Accept': 'application/json, text/plain, */*',
             'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
@@ -66,16 +69,20 @@ class XUIManager:
 
         try:
             response = self.session.post(f'http://{ip}:{self.port}/xui/inbound/add', headers=headers, data=data, verify=False)
-            print(response.text)
+            logging.info(f"{ip} 添加节点成功")
             return True
         except Exception as e:
-            print(f"{ip} 添加失败: {e}")
+            logging.error(f"{ip} 添加节点失败: {e}")
             return False
 
     def get_inbounds(self, ip, client_id):
         try:
             response = self.session.post(f'http://{ip}:{self.port}/xui/inbound/list', headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}, verify=False)
-            print(f"{ip} 获取成功")
+            if response.status_code != 200:
+                logging.error(f"{ip} 获取节点失败")
+                return
+            
+            logging.info(f"{ip} 获取节点成功")
 
             result = response.json()["obj"][-1]
             obj = {
@@ -95,22 +102,25 @@ class XUIManager:
             byte_str = json_str.encode('utf-8')
             base64_encoded_str = base64.b64encode(byte_str)
             decoded_str = base64_encoded_str.decode('utf-8')
-            print(f"vmess://{decoded_str}\n")
+            logging.info(f"vmess://{decoded_str}\n")
             with open('vmess.txt', 'a', encoding='utf-8') as f:
                 f.write(f"vmess://{decoded_str}\n")
             return True
         except Exception as e:
-            print(f"{ip} 获取失败: {e}")
+            logging.error(f"{ip} 获取失败: {e}")
             return False
 
 
 
 if __name__ == '__main__':
+    
     # 使用argparse添加命令行参数
-    parser = argparse.ArgumentParser(description="XUI Inbound Management Script")
-    parser.add_argument('remark', type=str, help="The remark to use for inbound entries")
+    parser = argparse.ArgumentParser(description="XUI 入站管理脚本")
+    parser.add_argument('--notes', type=str, default="dl", help="用于入站条目的备注，默认为'dl'")
+    parser.add_argument('--idx', type=int, default=0, help="入站条目的索引(默认值:0)")
     args = parser.parse_args()
-    remark = args.remark
+    notes = args.notes
+    idx = args.idx
 
     with open('ips.txt', 'r', encoding='utf-8') as f:
         ips_list = f.readlines()
@@ -119,7 +129,7 @@ if __name__ == '__main__':
             manager = XUIManager()
             if manager.login(ip):
                 client_id = str(uuid.uuid4())
-                manager.add_inbound(ip, remark, i, client_id)
+                manager.add_inbound(ip, notes, client_id, i+idx)
                 manager.get_inbounds(ip, client_id)
 
     print("done")
